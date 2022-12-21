66770c399337987f748fa0b90213bf26e36f3bde
SSBarik <ssbarik.sf@gmail.com>
2022-12-21 22:02:29 +0530

    style: nav menu item

--- src/components/navigation/SmallScreenNav.js
+++ src/components/navigation/SmallScreenNav.js
@@ -23,15 +23,23 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.primary.main,
     textDecoration: 'none'
   },
-  drawerContainer: {
-    padding: '20px 30px'
+
+  paper: {
+    padding: '30px 35px',
+    background: 'transparent',
+    backdropFilter:  `blur(10px)`,
+    border: '1px outset rgba(255, 255, 255, 0.2)',
+    borderRadius: '50px 0px 0px 50px'
+
   },
   active: {
     backgroundColor: theme.palette.primary.main,
-    color: '#ffffff',
-    fontWeight: 'bold',
-    borderRadius: '50px'
+  },
+
+  inactive: {
+    border: '2px outset #22b59d'
   }
+
 }));
 
 export default function SmallScreenNav({ drawerOpen, setState, config }) {
@@ -61,21 +69,24 @@ export default function SmallScreenNav({ drawerOpen, setState, config }) {
         anchor="right"
         open={drawerOpen}
         onClose={handleDrawerClose} 
+        classes={{ paper: classes.paper }}
       >
-        <div className={classes.drawerContainer}>
+        {/* <div className={classes.drawerContainer}> */}
           {config.map((item, i) => (
             <MenuItem 
               key={i} 
               exact 
               to={item.path}  
               component={NavLink} 
+              style={{marginBottom: '15px', fontSize: 'large', fontWeight: 'bold', color: '#ffffff', borderRadius: '50px'}}
+              className={classes.inactive}
               activeClassName={classes.active}
               onClick={handleDrawerClose}
             >
               {item.label}
             </MenuItem>
           ))} 
-        </div>
+        {/* </div> */}
       </Drawer>
     </>
   );
